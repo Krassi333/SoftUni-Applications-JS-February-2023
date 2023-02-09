8b43d33743450d7fcc81b6cf045da1ba4ef8baa6
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-02-09 16:51:36 +0200

    Add solution

--- JS Applications/Asynchronous Programming/02. Bus Schedule/app.js
+++ JS Applications/Asynchronous Programming/02. Bus Schedule/app.js
@@ -1,11 +1,40 @@
 function solve() {
+    let stopID = 'depo';
+    let stopName = '';
+    let infoBox = document.querySelector('#info span');
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${stopID}`;
+        try {
+            let res = await fetch(url);
+
+            if (res.status != 200) {
+                throw new Error;
+            }
+            let data = await res.json();
+            console.log(data);
+            stopID = data.next;
+            stopName = data.name;
+            infoBox.innerText = `Next stop ${stopName}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        } catch (error) {
+            infoBox.innerText = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
+
+
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.innerText = `Arriving at ${stopName}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+
     }
 
     return {
