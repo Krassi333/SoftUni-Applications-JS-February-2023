85e8a93767aee050c818e905da07d6eb513afc68
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-02-09 15:51:44 +0200

    Add solution

--- JS Applications/Asynchronous Programming/01. Bus Stop/app.js
+++ JS Applications/Asynchronous Programming/01. Bus Stop/app.js
@@ -1,3 +1,34 @@
 function getInfo() {
-    
+    let checkBtn = document.getElementById('submit');
+    checkBtn.addEventListener('click', async () => {
+        let id = document.getElementById('stopId').value;
+        let stopName = document.getElementById('stopName');
+        let busesList = document.getElementById('buses');
+
+        let url = `http://localhost:3030/jsonstore/bus/businfo/${id}`;
+
+        try {
+            busesList.replaceChildren();
+            let res = await fetch(url);
+
+            if (res.status != 200) {
+                throw new Error;
+            }
+
+            let data = await res.json();
+            stopName.innerText = `${data.name}`;
+
+            let busesInfo = data.buses;
+            console.log(busesInfo);
+
+            for (let [key, value] of Object.entries(busesInfo)) {
+                let li = document.createElement('li');
+                li.innerText = `Bus ${key} arrives in ${value} minutes`;
+                busesList.appendChild(li);
+            }
+        } catch (error) {
+            stopName.innerText = 'Error';
+        }
+
+    })
 }
