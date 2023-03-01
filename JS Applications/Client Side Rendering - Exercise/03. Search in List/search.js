d055d2f16525bd42db7899ffd45292e83e566cfd
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-01 22:49:24 +0200

    Add solution

--- JS Applications/Client Side Rendering - Exercise/03. Search in List/search.js
+++ JS Applications/Client Side Rendering - Exercise/03. Search in List/search.js
@@ -1,3 +1,37 @@
+import { towns } from './towns.js'
+import { html, render } from '/node_modules/lit-html/lit-html.js'
+
 function search() {
-   // TODO
+   console.log(towns);
+
+   let root = document.getElementById('towns');
+   let searchTextField = document.getElementById('searchText');
+   let searchText = searchTextField.value;
+
+   let res = (towns, searchText) => html`
+<ul>
+   ${towns.map(el => html`<li class=${searchText && el.toLowerCase().includes(searchText.toLowerCase()) ? 'active' : ''
+    }>${el}</li>`)}
+</ul>
+`
+   render(res(towns, searchText), root);
+
+   let searchBtn = document.querySelector('button');
+   searchBtn.addEventListener('click', () => {
+      searchText = searchTextField.value;
+      let count = 0;
+
+      towns.forEach(town => {
+         if (town.toLowerCase().includes(searchText.toLowerCase())) {
+            count++;
+         }
+
+      })
+      render(res(towns, searchText), root);
+
+      document.getElementById('result').innerText = `${count} matches found`
+
+   })
 }
+
+search()
