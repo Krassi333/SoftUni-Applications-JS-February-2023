c85b78679b2bab5d6f2a60b90d4285e8518d0f98
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-01 23:43:50 +0200

    Add solution

---
+++ JS Applications/Client Side Rendering - Exercise/04. Fill Dropdown/dropdown.js
@@ -0,0 +1,48 @@
+import { html, render } from '/node_modules/lit-html/lit-html.js'
+
+let url = 'http://localhost:3030/jsonstore/advanced/dropdown';
+
+async function loadData() {
+
+    let res = await fetch(url);
+    let rowData = await res.json();
+    let data = Object.values(rowData);
+
+    update(data);
+}
+
+loadData();
+
+function addItem() {
+
+    let addBtn = document.querySelector('input[type=submit]');
+    addBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+
+        let text = document.getElementById('itemText').value;
+        console.log(text);
+        let res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                text
+            })
+        })
+
+        if (res.status == 200) {
+            console.log(await res.json());
+            loadData();
+        }
+    })
+}
+addItem()
+
+function update(data) {
+    let root = document.getElementById('menu');
+    let optionList = (data) => html`
+    ${data.map(el => html`<option value=${el._id}>${el.text}</option>`)}
+    `
+    render(optionList(data), root)
+}
