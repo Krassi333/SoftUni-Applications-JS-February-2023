89652137b46d5bd0fc7f19f4319358ee4b5db78f
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-03 22:53:52 +0200

    Create edit.js

---
+++ JS Applications/Exercise Routing/01.Implement Furniture Store/views/edit.js
@@ -0,0 +1,66 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js'
+import { getItemDetails } from '../src/data.js';
+import page from '../node_modules/page/page.mjs'
+
+const root = document.getElementById('root');
+
+const template = (data) => html`
+<div class="row space-top">
+    <div class="col-md-12">
+        <h1>Edit Furniture</h1>
+        <p>Please fill all fields.</p>
+    </div>
+</div>
+<form>
+    <div class="row space-top">
+        <div class="col-md-4">
+            <div class="form-group">
+                <label class="form-control-label" for="new-make">Make</label>
+                <input class="form-control" id="new-make" type="text" name="make" value=${data.make}>
+            </div>
+            <div class="form-group has-success">
+                <label class="form-control-label" for="new-model">Model</label>
+                <input class="form-control" id="new-model" type="text" name="model" value=${data.model}>
+            </div>
+            <div class="form-group has-danger">
+                <label class="form-control-label" for="new-year">Year</label>
+                <input class="form-control" id="new-year" type="number" name="year" value=${data.year}>
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="new-description">Description</label>
+                <input class="form-control" id="new-description" type="text" name="description"
+                    value=${data.description}>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="form-group">
+                <label class="form-control-label" for="new-price">Price</label>
+                <input class="form-control" id="new-price" type="number" name="price" value=${data.price}>
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="new-image">Image</label>
+                <input class="form-control" id="new-image" type="text" name="img" value=${data.img}>
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="new-material">Material (optional)</label>
+                <input class="form-control" id="new-material" type="text" name="material" value=${data.material}>
+            </div>
+            <input type="submit" @click=${onSubmit} class="btn btn-info" value="Edit" />
+        </div>
+    </div>
+</form>
+`
+
+export async function showEdit(ctx) {
+
+    const id = ctx.params.itemId;
+    let data = await getItemDetails(id);
+    console.log(data);
+    render(template(data), root);
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+
+
+}
