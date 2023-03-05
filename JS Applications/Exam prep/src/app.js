0cfb6805856b505a35b9c2d3b3441112e5ec6901
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-05 22:26:43 +0200

    Add app .js

---
+++ JS Applications/Exam prep/src/app.js
@@ -0,0 +1,36 @@
+import page from '../node_modules/page/page.mjs';
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { loginView } from './views/login.js';
+import { catalogView } from './views/catalog.js';
+import { registerView } from './views/register.js';
+import { detailsView } from './views/details.js';
+
+const root = document.getElementById('site-content');
+
+function decoratectx(ctx, next) {
+    ctx.render = (htmlResult) => render(htmlResult, root);
+    ctx.uppdateNavBar = uppdateNavBar;
+    next();
+}
+
+function uppdateNavBar() {
+    let user = JSON.parse(localStorage.getItem('user'));
+
+    if (user) {
+        document.getElementById('guest').style.display = 'none';
+        document.getElementById('user').style.display = 'inline-block';
+        document.querySelector('#user span').textContent = `Welcome, ${user.email}`;
+    } else {
+        document.getElementById('guest').style.display = 'inline-block';
+        document.getElementById('user').style.display = 'none';
+    }
+}
+
+page(decoratectx);
+
+page('/', catalogView);
+page('/login', loginView);
+page('/register', registerView);
+page('/details', detailsView);
+
+page.start();
