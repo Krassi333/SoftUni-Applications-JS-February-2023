39dc0d1f7a6a90897d2a61728de9278d033a2bda
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-05 22:27:06 +0200

    Add homepage

---
+++ JS Applications/Exam prep/src/views/catalog.js
@@ -0,0 +1,38 @@
+import { html } from '../../node_modules/lit-html/lit-html.js'
+import { catalog } from '../data.js'
+
+const template = (data) => html`
+<section id="dashboard-page" class="dashboard">
+    <h1>Dashboard</h1>
+    ${data.length == 0 ?
+    html`<p class="no-books">No books in database!</p>`
+    : html`<ul class="other-books-list">
+        ${data.map(el => html`
+        <li class="otherBooks">
+            <h3>${el.title}</h3>
+            <p>Type: ${el.type}</p>
+            <p class="img"><img src=${el.imageUrl}></p>
+            <a class="button" href="/details">Details</a>
+        </li>
+        `)}
+    </ul>
+    `
+    }
+
+
+
+    <!-- Display paragraph: If there are no books in the database -->
+
+</section>
+`
+
+export async function catalogView(ctx) {
+   
+        let data = await catalog();
+        console.log(data);
+        
+        ctx.render(template(data));
+        
+
+
+}
