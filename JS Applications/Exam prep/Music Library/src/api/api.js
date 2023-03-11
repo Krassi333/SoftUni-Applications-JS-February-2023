f1e9230ccc2eb1ba3355989bd491a57c6d6cfde6
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-11 13:59:38 +0200

    Create api.js

---
+++ JS Applications/Exam prep/Music Library/src/api/api.js
@@ -0,0 +1,57 @@
+import { getUser } from './util.js';
+
+const baseURL = 'http://localhost:3030';
+
+
+ async function request(url, options) {
+    try {
+        let res = await fetch(baseURL + url, options);
+
+        if (res.ok == false) {
+            let err = await res.json();
+            throw new Error(err.message);
+        }
+
+        if (res.status == 204) {
+            return res;
+        } else {
+            return await res.json();
+        }
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+ async function getOptions(method, body) {
+    let option = {
+        method,
+        headers: {}
+    }
+
+    if (body) {
+        option.body = JSON.stringify(body);
+        option.headers['Content-Type'] = 'application/json';
+    }
+
+    const user = getUser();
+
+    if (user) {
+        option.headers['X-Authorization'] = user.accessToken;
+    }
+}
+
+export async function get(url) {
+    return request(url, getOptions('GET'));
+}
+
+export async function put(url, data) {
+    return request(url, getOptions('PUT', data));
+}
+
+export async function post(url, data) {
+    return request(url, getOptions('POST', data));
+}
+
+export async function del(url) {
+    return request(url, options('DELETE'));
+}
