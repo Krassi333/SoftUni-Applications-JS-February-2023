13fb4a28b69d512b672cacb7170dd9defe3600f1
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-16 10:27:03 +0200

    Add util.js

---
+++ JS Applications/Exam prep/Clear Career/src/api/util.js
@@ -0,0 +1,15 @@
+export  function getUser() {
+    let user = localStorage.getItem('user');
+    
+    if (user) {
+        return JSON.parse(user);
+    }
+}
+
+export  function setUser(data) {
+    localStorage.setItem('user', JSON.stringify(data));
+}
+
+export  function clearUser() {
+    localStorage.removeItem('user');
+}
