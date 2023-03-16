f6ec5044e00d49fda4ab00b1ea897e2a58900239
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-16 10:45:32 +0200

    Add register.js

---
+++ JS Applications/Exam prep/Clear Career/src/view/register.js
@@ -0,0 +1,41 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { register } from '../api/data.js';
+import { validateRegisterData } from '../api/validate.js';
+
+const template = (onSubmit) => html`
+<section id="register">
+    <div class="form">
+        <h2>Register</h2>
+        <form class="login-form" @submit=${onSubmit}>
+            <input type="text" name="email" id="register-email" placeholder="email" />
+            <input type="password" name="password" id="register-password" placeholder="password" />
+            <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
+            <button type="submit">register</button>
+            <p class="message">Already registered? <a href="/login">Login</a></p>
+        </form>
+    </div>
+</section>
+`
+
+export async function registerView(ctx) {
+    ctx.render(template(onSubmit));
+
+    async function onSubmit(e) {
+        e.preventDefault();
+
+        let formData = new FormData(e.target);
+
+        let data = {
+            email: formData.get('email'),
+            password: formData.get('password'),
+            rePass: formData.get('re-password')
+        }
+
+
+        if (validateRegisterData(data)) {
+            await register(data.email, data.password);
+            ctx.updateNavBar();
+            ctx.page.redirect('/catalog');
+        }
+    }
+}
