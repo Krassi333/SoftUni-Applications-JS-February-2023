75f07721765722a6ffc15284d2d2c305c158b724
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-17 13:08:37 +0200

    Create app.js

---
+++ JS Applications/Exam prep/Templates/app.js
@@ -0,0 +1,56 @@
+import { render } from '../node_modules/lit-html/lit-html.js';
+import page from '../node_modules/page/page.mjs';
+import { logout } from './api/data.js';
+import { addOfferView } from './view/addOffer.js';
+import { catalogView } from './view/catalog.js';
+import { detailsView } from './view/details.js';
+import { editView } from './view/edit.js';
+import { homeView } from './view/home.js';
+import { loginView } from './view/login.js';
+import { registerView } from './view/register.js';
+
+
+
+const root = document.querySelector('main');
+
+function decorateContext(ctx, next) {
+    ctx.render = (htmlResponce) => render(htmlResponce, root);
+    ctx.updateNavBar = updateNavBar;
+    next();
+}
+
+function updateNavBar() {
+    //debugger
+    let user = localStorage.getItem('user');
+    let userSection = document.querySelector('div.user');
+    let guestSection = document.querySelector('div.guest');
+
+    if (user) {
+        userSection.style.display = 'inline-block';
+        guestSection.style.display = 'none';
+    } else {
+        userSection.style.display = 'none';
+        guestSection.style.display = 'inline-block';
+    }
+}
+
+updateNavBar();
+page(decorateContext);
+
+page.redirect('/');
+
+page('/login', loginView);
+page('/', homeView);
+page('/register', registerView);
+page('/catalog', catalogView);
+page('/logout', async () => {
+    await logout();
+    page.redirect('/catalog');
+    updateNavBar();
+
+});
+page('/addOffer', addOfferView);
+page('/details/:id', detailsView);
+page('/edit/:id', editView);
+
+page.start();
