c41996c92dfe210dda0da8f781e60acded86d19b
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-03-30 10:52:37 +0300

    Create util.js

---
+++ JS Applications/Exam prep/Templates/Music Library/src/api/util.js
@@ -0,0 +1,17 @@
+export  function getUser() {
+    
+    let user = localStorage.getItem('user');
+    
+    if (user) {
+        return JSON.parse(user);
+    }
+}
+
+
+export  function setUser(data) {
+    localStorage.setItem('user', JSON.stringify(data));
+}
+
+export  function clearUser() {
+    localStorage.removeItem('user');
+}
